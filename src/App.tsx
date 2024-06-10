eee7b1187bc2e79f127efa5ddb895f8cbc036b2d
ILzat <ilzat.sadykov.2006@bk.ru>
2024-06-10 16:52:44 +0600

    'Complete level 1 Сделал доп задание добавил footer в модалку сделал интерфейс для него, вызываю у него map и прохожусь по массиву обьектов в app'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import {useState} from "react";
-import Modal from "./Components/Modal/Modal.tsx";
+import {useState} from 'react';
+import Modal from './Components/Modal/Modal.tsx';
 
 const App = () => {
-    const [showModal, setShowModal] = useState(false);
-    return (
-        <>
-            <button
-                className={'btn btn-primary ms-5 mt-5'}
-                onClick={() => setShowModal(true)}>
-                Order
-            </button>
-            <Modal
-                show={showModal}
-                title={' Modal Order Tittle'}
-                onClose={() => setShowModal(false)}
-            >
-                <p className={'modal-body'}>
-                    Content
-                </p>
-            </Modal>
-        </>
-    )
+  const btnSettings = [
+    {type: 'primary', label: 'Continue', onClick: () => console.log('clicked continue')},
+    {type: 'danger', label: 'Close', onClick: () => console.log('clicked cancel')}
+  ];
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <button
+        className={'btn btn-primary ms-5 mt-5'}
+        onClick={() => setShowModal(true)}>
+        click me!
+      </button>
+      <Modal
+        show={showModal}
+        title={' Modal Order Tittle'}
+        onClose={() => setShowModal(false)}
+        buttons={btnSettings}
+      >
+        <p className={'modal-body'}>
+          Content
+        </p>
+        <div className="modal-footer">
+
+        </div>
+      </Modal>
+    </>
+  );
 };
 
-export default App
+export default App;
--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+interface ButtonProps {
+  type: string;
+  label: string;
+  onClick: () => void;
+}
+
 interface Props extends React.PropsWithChildren {
   show: boolean;
   title: string;
   onClose: React.MouseEventHandler;
+  buttons: ButtonProps[];
 }
 
-const Modal: React.FC<Props> = ({show, title, onClose, children}) => {
+const Modal: React.FC<Props> = ({show, title, onClose, buttons, children}) => {
   return (
     <>
       <div
@@ -14,10 +21,14 @@ const Modal: React.FC<Props> = ({show, title, onClose, children}) => {
         style={{display: show ? 'block' : 'none'}}/>
       <div
         className="modal show"
-        style={{display: show ? 'block' : 'none'}}>
+        style={{display: show ? 'block' : 'none'}}
+        onClick={onClose}
+      >
         <div className="modal-dialog">
           <div
-            className="modal-content">
+            className="modal-content"
+            onClick={event => event.stopPropagation()}
+          >
             <div className="modal-header">
               <h1 className="modal-title fs-5">{title}</h1>
               <button className={'btn ms-auto'} onClick={onClose}>
@@ -25,6 +36,13 @@ const Modal: React.FC<Props> = ({show, title, onClose, children}) => {
               </button>
             </div>
             {children}
+            <div className={'modal-footer'}>
+              {buttons.map((btn, index) => (
+                <button key={index} className={`btn btn-${btn.type} `} onClick={btn.onClick}>
+                  {btn.label}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
       </div>
